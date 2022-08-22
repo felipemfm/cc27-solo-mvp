916063bea1d30c907a76d5bd03cd665807f753a6
felipemfm <felipemfm@gmail.com>
2022-08-22 14:56:21 +0900

    Feat: Add userNameGenerator function

---
+++ src/util/userNameGenerator.ts
@@ -0,0 +1,38 @@
+export function userNameGenerator(): { name: string; token: string } {
+  const date = new Date();
+  const animalAvatar = [
+    "Cat",
+    "Dog",
+    "Lizard",
+    "Lion",
+    "Elephant",
+    "Hippo",
+    "Parrot",
+    "Toucans",
+    "Dolphin",
+    "Whale",
+    "Monkey",
+    "Fish",
+    "Koala",
+  ];
+  const adAvatar = [
+    "agile",
+    "nimble",
+    "spry",
+    "strong",
+    "secure",
+    "powerful",
+    "intelligent",
+    "clever",
+    "bright",
+    "sharp",
+  ];
+
+  const name = `${adAvatar[Math.floor(Math.random() * adAvatar.length)]}${
+    animalAvatar[Math.floor(Math.random() * animalAvatar.length)]
+  }`;
+
+  const token = date.getTime().toString(36).toUpperCase();
+
+  return { name, token };
+}
