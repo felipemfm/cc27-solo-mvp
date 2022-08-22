1858b8fae13cdaa8a9353ef3e3a710f5c8945cff
felipemfm <felipemfm@gmail.com>
2022-08-22 17:19:56 +0900

    Feat: Add message send functionally

--- src/components/MessageInput.tsx
+++ src/components/MessageInput.tsx
@@ -1,7 +1,40 @@
+import { read } from "fs";
+import React, { useEffect, useRef, useState } from "react";
 import "./MessageInput.css";
 
-const MessageInput = () => {
-    return <div className="Input">MessageInput</div>
+interface props {
+  user: {
+    name: string;
+    token: string;
+  } | null;
 }
+
+const MessageInput: React.FC<props> = (props) => {
+  const [message, setMessage] = useState<string>("");
+  const [readyMessage, setReadyMessage] = useState<string | null>(null);
+
+  const sendMessage = () => {
+    if (message) {
+      setReadyMessage(message);
+      setMessage("");
+    }
+  };
+
+  useEffect(() => {
+    console.log({ ...props.user, message: readyMessage });
+  }, [readyMessage]);
+
+  return (
+    <div className="Input">
+      <input
+        type="text"
+        id="TextInput"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button onClick={sendMessage}>Send</button>
+    </div>
+  );
+};
 
 export default MessageInput;
